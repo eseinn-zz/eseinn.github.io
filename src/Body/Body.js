14883106ef449498e67f2d6321163f16bd052bd7
Einar Smári Einarsson <einar.smari.einarsson@origo.is>
2020-12-05 16:48:19 +0000

    Add tex to show today and countdown until christmas.

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,8 +1,28 @@
+import styled from 'styled-components';
 import { Calendar } from './';
+
+const TodayDate = styled.div``;
+const CountdownDays = styled.div``;
+
+const today = new Date();
+const dayOfMonth = today.getDate();
+
+const getTodayDateString = () => {
+  const formattedDate =
+    dayOfMonth + '.' + (today.getMonth() + 1) + '.' + '2020';
+  return formattedDate;
+};
+
+const numberOfDaysTilChristmas = () => {
+  return 24 - dayOfMonth;
+};
 const Body = () => {
   return (
     <div>
-      Body!
+      <TodayDate>Í dag er {getTodayDateString()}</TodayDate>
+      <CountdownDays>
+        og það eru {numberOfDaysTilChristmas()} dagar til jóla!
+      </CountdownDays>
       <Calendar />
     </div>
   );
